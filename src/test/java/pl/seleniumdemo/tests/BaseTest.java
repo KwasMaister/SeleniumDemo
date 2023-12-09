67828134a3c1d83450e5a7431915c313af1f7e1b
Kuba <jakubx200@gmail.com>
2023-12-09 14:07:44 +0100

    Add BeforeSuite and AfterSuite

--- src/test/java/pl/seleniumdemo/tests/BaseTest.java
+++ src/test/java/pl/seleniumdemo/tests/BaseTest.java
@@ -1,13 +1,33 @@
-package pl.seleniumdemo.pages;
+package pl.seleniumdemo.tests;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 import pl.seleniumdemo.utils.DriverFactory;
 
 public class BaseTest {
 
     protected WebDriver driver;
+    protected static ExtentHtmlReporter htmlReporter;
+    protected static ExtentReports extentReports;
+
+    @BeforeSuite
+    public void beforeSuite () {
+        htmlReporter = new ExtentHtmlReporter("index.html");
+        extentReports = new ExtentReports();
+        extentReports.attachReporter(htmlReporter);
+    }
+
+    @AfterSuite
+    public void afterSuite () {
+        htmlReporter.flush();
+        extentReports.flush();
+    }
 
     @BeforeMethod
     public void setup(){
@@ -18,6 +38,6 @@ public class BaseTest {
 
     @AfterMethod
     public void tearDown(){
-        driver.quit();
+       // driver.quit();
     }
 }
