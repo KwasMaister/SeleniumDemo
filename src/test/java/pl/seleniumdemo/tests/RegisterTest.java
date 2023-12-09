931d587b0ccd70203109ec400cff81c7aa94eed5
Kuba <jakubx200@gmail.com>
2023-12-09 14:07:03 +0100

    Add RegisterTest

---
+++ src/test/java/pl/seleniumdemo/tests/RegisterTest.java
@@ -0,0 +1,114 @@
+package pl.seleniumdemo.tests;
+
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
+import com.aventstack.extentreports.Status;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pl.seleniumdemo.pages.HomePage;
+import pl.seleniumdemo.utils.SeleniumHelper;
+
+import java.io.IOException;
+
+public class RegisterTest extends BaseTest {
+
+    @Test
+    public void registerValidUserTest() {
+
+        int randomNumber = (int) (Math.random()*10000);
+
+        String getWelcomeText = new HomePage(driver).openMyAccountPage()
+                .registerUserValidDate("nazwa" + randomNumber + "@nazwa.pl", "123DudixPatrzy123!#")
+                .getWelcomeText()
+                .getText();
+
+        Assert.assertTrue(getWelcomeText.contains("Hello nazwa" + randomNumber));
+    }
+
+    @Test
+    public void registerUserWithTheSameEmailAddresTest() {
+        WebElement getError =  new HomePage(driver).openMyAccountPage()
+                .registerUserInvalidDate("test123@com.pl", "test123@com.pl")
+                .getError();
+
+        Assert.assertEquals("Error: An account is already registered with your email address. Please log in.", getError.getText());
+
+    }
+
+    @Test
+    public void registerUserWithInvalidEmailAddress() {
+            WebElement getError = new HomePage(driver).openMyAccountPage()
+                    .registerUserInvalidDate("test123@test", "test123@com.pl")
+                    .getError();
+
+            Assert.assertEquals("Error: Please provide a valid email address.", getError.getText());
+    }
+
+    @Test
+    public void registerUserWithVeryWeakPassword () {
+
+        int randomNumber = (int) (Math.random() * 1000);
+
+        String passwordError = new HomePage(driver).openMyAccountPage()
+                .checkingPassword("emailtest" + randomNumber + "@com.pl", "123456789")
+                .getPasswordErrorVeryWeak()
+                .getText();
+
+        Assert.assertEquals(passwordError, "Very weak - Please enter a stronger password.");
+
+    }
+
+    @Test
+    public void registerUserWithWeakPassword () {
+        int randomNumber = (int) (Math.random() * 1000);
+
+        String passwordError = new HomePage(driver).openMyAccountPage()
+                .checkingPassword("emailtest" + randomNumber + "@com.pl", "123!@#")
+                .getPasswordErrorWeak()
+                .getText();
+
+        Assert.assertEquals(passwordError, "Weak - Please enter a stronger password.");
+    }
+
+    @Test
+    public void registerUserWithMediumPassword () {
+        int randomNumber = (int) (Math.random() * 1000);
+
+        String passwordError = new HomePage(driver).openMyAccountPage()
+                .checkingPassword("emailtest" + randomNumber + "@com.pl", "123!@#123")
+                .getPasswordMedium()
+                .getText();
+
+        Assert.assertEquals(passwordError, "Medium");
+    }
+
+    @Test
+    public void registerUserWithStrongPassword () {
+        int randomNumber = (int) (Math.random() * 1000);
+
+        String passwordError = new HomePage(driver).openMyAccountPage()
+                .checkingPassword("emailtest" + randomNumber + "@com.pl", "123!@#ABCabc")
+                .getPasswordStrong()
+                .getText();
+
+        Assert.assertEquals(passwordError, "Strong");
+    }
+
+    @Test
+    public void checkingHintAboutPassword () throws IOException {
+
+        ExtentTest test = extentReports.createTest("Checking Hit About Password");
+        int randomNumber = (int) (Math.random() * 1000);
+
+        String hintAboutPassword = new HomePage(driver).openMyAccountPage()
+                .checkingPassword("emailtest" + randomNumber + "@com.pl", "123456789")
+                .getHintAboutPassword()
+                .getText();
+
+        test.log(Status.PASS, "Setting bad Password");
+        Assert.assertTrue(hintAboutPassword.contains("Hint: The password should be at least twelve characters long."));
+        test.log(Status.PASS, "Assertions passed", SeleniumHelper.getScreenshot(driver));
+    }
+}
