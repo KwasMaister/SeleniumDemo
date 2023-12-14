52d1f2f8c2ed4c3e78ceaa695f39d78729ce96fb
Kuba <jakubx200@gmail.com>
2023-12-14 19:49:42 +0100

    Add Page Object

--- src/test/java/pl/seleniumdemo/pages/MyAccountPage.java
+++ src/test/java/pl/seleniumdemo/pages/MyAccountPage.java
@@ -4,54 +4,55 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
 
 public class MyAccountPage {
 
     private WebDriver driver;
 
-    public MyAccountPage (WebDriver driver){
-        PageFactory.initElements(driver, this);
+    public MyAccountPage(WebDriver driver) {
+        PageFactory.initElements(new AjaxElementLocatorFactory(driver, 5), this);
         this.driver = driver;
     }
 
-    @FindBy (id = "reg_email")
+    @FindBy(id = "reg_email")
     private WebElement regEmailAddress;
 
-    @FindBy (id = "reg_password")
+    @FindBy(id = "reg_password")
     private WebElement regPassword;
 
-    @FindBy (name = "register")
+    @FindBy(name = "register")
     private WebElement regButton;
 
-    @FindBy (id = "username")
+    @FindBy(id = "username")
     private WebElement logUserNameOrEmailAddress;
 
-    @FindBy (id = "password")
+    @FindBy(id = "password")
     private WebElement logPassword;
 
-    @FindBy (name = "login")
+    @FindBy(name = "login")
     private WebElement logButton;
 
-    @FindBy (xpath = "//ul[@class='woocommerce-error']//li")
+    @FindBy(xpath = "//ul[@class='woocommerce-error']//li")
     private WebElement error;
 
-    @FindBy (xpath = "//div[@class='woocommerce-password-strength short']")
+    @FindBy(xpath = "//div[@class='woocommerce-password-strength short']")
     private WebElement passwordErrorVeryWeak;
 
-    @FindBy (xpath = "//div[@class='woocommerce-password-strength bad']")
+    @FindBy(xpath = "//div[@class='woocommerce-password-strength bad']")
     private WebElement passwordErrorWeak;
 
-    @FindBy (xpath = "//div[@class='woocommerce-password-strength good']")
+    @FindBy(xpath = "//div[@class='woocommerce-password-strength good']")
     private WebElement passwordMedium;
 
-    @FindBy (xpath = "//div[@class='woocommerce-password-strength strong']")
+    @FindBy(xpath = "//div[@class='woocommerce-password-strength strong']")
     private WebElement passwordStrong;
 
-    @FindBy (xpath = "//small[@class='woocommerce-password-hint']")
+    @FindBy(xpath = "//small[@class='woocommerce-password-hint']")
     private WebElement hintAboutPassword;
 
     public WebElement getError() {
-        return  error;
+        return error;
     }
 
     public LoggedUserPage registerUserValidDate(String email, String password) {
@@ -70,11 +71,26 @@ public class MyAccountPage {
         regButton.click();
     }
 
-    public MyAccountPage checkingPassword (String email, String passowrd) {
+    public MyAccountPage checkingPassword(String email, String passowrd) {
         regEmailAddress.sendKeys(email);
         regPassword.sendKeys(passowrd);
-        regButton.click();
         return this;
+    }
+
+    public LoggedUserPage loginUserValidDate(String emailOrName, String password) {
+        loginUser(emailOrName, password);
+        return new LoggedUserPage(driver);
+    }
+
+    public MyAccountPage loginUserInvalidDate(String emailOrName, String password) {
+        loginUser(emailOrName, password);
+        return this;
+    }
+
+    private void loginUser(String emailOrName, String password) {
+        logUserNameOrEmailAddress.sendKeys(emailOrName);
+        logPassword.sendKeys(password);
+        logButton.click();
     }
 
     public WebElement getPasswordErrorVeryWeak() {
@@ -85,16 +101,17 @@ public class MyAccountPage {
         return passwordErrorWeak;
     }
 
-    public WebElement getPasswordMedium () {
+    public WebElement getPasswordMedium() {
         return passwordMedium;
     }
 
-    public WebElement getPasswordStrong () {
+    public WebElement getPasswordStrong() {
         return passwordStrong;
     }
 
-    public WebElement getHintAboutPassword () {
+    public WebElement getHintAboutPassword() {
         return hintAboutPassword;
     }
+
 
 }
