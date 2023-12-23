f8a7112b3023fc9d0723c568658f4f32264bd0a3
Kuba <jakubx200@gmail.com>
2023-12-23 16:32:24 +0100

    Add Models Customer for test checkoutTest

---
+++ src/test/java/pl/seleniumdemo/models/Customer.java
@@ -0,0 +1,95 @@
+package pl.seleniumdemo.models;
+
+public class Customer {
+
+    private String firstName = "Kuba";
+    private String lastName = "Testowy";
+    private String companyName = "Test Company";
+    private String country = "Poland";
+    private String street = "Ulica";
+    private String flatNumber = "169";
+    private String zipCode = "04267";
+    private String city = "Cieszyn";
+    private String phone = "123123123";
+    private String email = "abcd@testowe.com.pl";
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getFlatNumber() {
+        return flatNumber;
+    }
+
+    public void setFlatNumber(String flatNumber) {
+        this.flatNumber = flatNumber;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
