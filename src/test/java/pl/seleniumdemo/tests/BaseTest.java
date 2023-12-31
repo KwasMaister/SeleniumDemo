4133078f4b8c15b7d68d16297c4b07e8279b4fd4
Kuba <jakubx200@gmail.com>
2023-12-31 15:56:41 +0100

    Reformat Code

--- src/test/java/pl/seleniumdemo/tests/BaseTest.java
+++ src/test/java/pl/seleniumdemo/tests/BaseTest.java
@@ -2,16 +2,13 @@ package pl.seleniumdemo.tests;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
-import com.aventstack.extentreports.Status;
-import com.aventstack.extentreports.markuputils.Markup;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import org.openqa.selenium.WebDriver;
-import org.testng.ITestResult;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 import pl.seleniumdemo.utils.DriverFactory;
-import pl.seleniumdemo.utils.SeleniumHelper;
-
-import java.io.IOException;
 
 public class BaseTest {
 
@@ -23,6 +20,7 @@ public class BaseTest {
 
     @BeforeSuite
     public void beforeSuite() {
+
         htmlReporter = new ExtentHtmlReporter("index.html");
         extentReports = new ExtentReports();
         extentReports.attachReporter(htmlReporter);
@@ -43,8 +41,7 @@ public class BaseTest {
 
     @AfterMethod
     public void tearDown() {
-
-        driver.quit();
+        DriverFactory.quitBrowser();
     }
 
 }
